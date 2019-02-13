570abf07de41a9666481e601dcde5db92edd4081
kriti <kritirajsinghi@gmail.com>
2019-02-13 07:31:25 -0500

    minor change

--- helpers/errorHandler.js
+++ helpers/errorHandler.js
@@ -4,12 +4,6 @@ function errorHandler(err, req, res, next) {
         // custom application error
         return res.status(400).json({ message: err });
     }
-
-    // if (err.name === 'UnauthorizedError') {
-    //     // jwt authentication error
-    //     return res.status(401).json({ message: 'Invalid Token' });
-    // }
-
     // default to 500 server error
     return res.status(500).json({ message: err.message });
 }
